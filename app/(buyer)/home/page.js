2c068f3efa58f99f14e155409e4caff11d798c60
GitPrasanth05 <155186304+GitPrasanth05@users.noreply.github.com>
2025-07-18 14:37:25 +0530

    added new things

--- app/(buyer)/home/page.js
+++ app/(buyer)/home/page.js
@@ -9,24 +9,57 @@ const Home = () => {
   const [data, setData] = useState([]);
   const [loading, setloading] = useState(true);
   const [back, setBack] = useState(0);
+  const [apii, setapii] = useState(true);
 
+  // useEffect(() => {
+  //   const fetchFromLocalStorage = async () => {
+  //     try {
+  //       let saved = localStorage.getItem("movies");
+  //       if (saved === null) {
+  //         const response = await fetch(
+  //           "https://api.themoviedb.org/3/movie/popular?api_key=2f1acd7e340ad453cb4ff5e205b57476"
+  //         );
+  //       }
+  //       if (saved) {
+  //         const parsed = JSON.parse(saved);
+  //         setData(parsed);
+  //       } else {
+  //         console.warn("No data found in localStorage");
+  //       }
+  //     } catch (error) {
+  //       console.error("Error reading from localStorage", error);
+  //     } finally {
+  //       setloading(false);
+  //     }
+  //   };
+
+  //   fetchFromLocalStorage();
+  // }, []);
   useEffect(() => {
-    const fetchFromLocalStorage = () => {
+    const fetchFromLocalStorage = async () => {
       try {
-        const saved = localStorage.getItem("movies");
-        if (saved) {
+        let saved = localStorage.getItem("movies");
+        if (saved === null) {
+          const response = await fetch(
+            "https://api.themoviedb.org/3/movie/popular?api_key=2f1acd7e340ad453cb4ff5e205b57476"
+          );
+          const data = await response.json();
+          localStorage.setItem("movies", JSON.stringify(data.results));
+          setData(data.results);
+        } else {
+          setapii(false);
           const parsed = JSON.parse(saved);
           setData(parsed);
-        } else {
-          console.warn("No data found in localStorage");
         }
       } catch (error) {
-        console.error("Error reading from localStorage", error);
+        console.error(
+          "Error reading from localStorage or fetching data",
+          error
+        );
       } finally {
         setloading(false);
       }
     };
-
     fetchFromLocalStorage();
   }, []);
 
@@ -39,14 +72,20 @@ const Home = () => {
     }
   }, [data]);
 
-  if (loading) {
+  if (loading && !apii) {
     return (
       <>
         <h1>Loading movies from localStorage...</h1>
       </>
     );
   }
-
+  if (loading && apii) {
+    return (
+      <>
+        <h1>Loading movies from API..</h1>
+      </>
+    );
+  }
   return (
     <>
       <Navbar />
